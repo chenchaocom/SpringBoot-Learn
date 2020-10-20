f35f9d411c10aebe9d7c3e32946809af41e3a016
chenchaocom <1845360542@qq.com>
2020-10-20 16:50:22 +0800

    Update HelloController.java

--- labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
+++ labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     public String say(){
-        return "FAIL";
+        return "SUCCESS";
     }
 }
