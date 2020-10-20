07a6ffd3d28d2838101a1ce6d7475627f21ed54e
陈超 <chen.chao02@opg.cn>
2020-10-20 16:43:19 +0800

    HelloController代码提交

--- labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
+++ labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
@@ -1,4 +1,10 @@
 package com.opglabel.springcloud.labx04.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    public String say(){
+        return "SUCCESS";
+    }
 }
