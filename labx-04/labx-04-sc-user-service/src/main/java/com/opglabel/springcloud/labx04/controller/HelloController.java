90d0e87a93bdfadf80204c6fb103b4bdbd71265c
陈超 <chen.chao02@opg.cn>
2020-10-20 17:13:03 +0800

    master修改

--- labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
+++ labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     public String say(){
-        System.out.println("master修改");
+        System.out.println("master修改1212");
         return "SUCCESS";
     }
 }
