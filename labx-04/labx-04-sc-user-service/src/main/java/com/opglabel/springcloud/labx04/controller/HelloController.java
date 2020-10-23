4662a2061804213e614e1e3b86497a3fb3b597d9
陈超 <chen.chao02@opg.cn>
2020-10-23 08:40:50 +0800

    添加循环

--- labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
+++ labx-04/labx-04-sc-user-service/src/main/java/com/opglabel/springcloud/labx04/controller/HelloController.java
@@ -2,11 +2,27 @@ package com.opglabel.springcloud.labx04.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
     public String say(){
         System.out.println("master修改1212");
         System.out.println("master修改1213");
         return "SUCCESS";
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Map<String,String> map = new HashMap<String, String>();
+
+        for(int i = 0;i<40000;i++){
+            map.put("1001"+i,"Umai:PROG/4336301@BESTV.SMG.SMG");
+        }
+        System.out.println("aa");
+        Thread.sleep(10000L);
+
     }
 }
